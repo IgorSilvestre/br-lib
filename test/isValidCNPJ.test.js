bc041edf9c0315380b714efc48252b93f117993d
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-11-07 16:43:30 -0300

    test: added isValidCNPJ unit tests

---
+++ test/isValidCNPJ.test.js
@@ -0,0 +1,23 @@
+import { isValidCNPJ } from "../isValidCNPJ";
+
+describe('isValidCNPJ', () => {
+  test('valid CNPJ should return true', () => {
+    const validCNPJ = '08.987.585/0001-97';
+    expect(isValidCNPJ(validCNPJ)).toBe(true);
+  });
+
+  test('CNPJ with all same digits should return false', () => {
+    const invalidCNPJ = '11.111.111/1111-11';
+    expect(isValidCNPJ(invalidCNPJ)).toBe(false);
+  });
+
+  test('CNPJ with incorrect verification digits should return false', () => {
+    const invalidCNPJ = '12.345.678/0001-91';
+    expect(isValidCNPJ(invalidCNPJ)).toBe(false);
+  });
+
+  test('CNPJ with incorrect format should return false', () => {
+    const invalidCNPJ = '12345678/0001-90';
+    expect(isValidCNPJ(invalidCNPJ)).toBe(false);
+  });
+});
