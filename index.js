4aaaa37371ab2771bc4366e2dcbd89c74ccfd70c
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-11-20 18:03:48 -0300

    refactor: default exports

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { isValidCNPJ } from "./functions/isValidCNPJ"
+import { insistFetch } from "./functions/insistFetch"
+import { scrapeWebsiteForContacts } from "./functions/scrapeWebsiteForContacts"
+import { parseISODateToBrazilSTD } from "./functions/parseISODateToBrazilSTD"
+import { regexForSearch } from "./functions/regexForSearch"
+
+export default {
+  isValidCNPJ,
+  insistFetch,
+  scrapeWebsiteForContacts,
+  parseISODateToBrazilSTD,
+  regexForSearch
+}
