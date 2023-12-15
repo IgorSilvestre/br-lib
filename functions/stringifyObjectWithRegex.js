fd71097355f42bc42c1cd2fbe139bda7d44b108e
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-12-15 15:48:19 -0300

    Add stringifyWithRegex function to convert objects to JSON with support for regular expressions

---
+++ functions/stringifyObjectWithRegex.js
@@ -0,0 +1,17 @@
+/**
+ * Converts an object to a JSON string, with support for regular expressions.
+ *
+ * @param {Object} obj - The object to be converted to a JSON string.
+ * @returns {string} - The JSON string representation of the object, with regular expressions converted to strings.
+ */
+export function stringifyWithRegex(obj) {
+  function replacer(key, value) {
+      if (value instanceof RegExp) {
+          // Convert RegExp to a string
+          return `__REGEXP ${value.toString()}`;
+      }
+      return value;
+  }
+
+  return JSON.stringify(obj, replacer);
+}
