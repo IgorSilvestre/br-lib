eee613fb329de2266fcac39787f9d5cd494f27e1
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-12-15 15:49:08 -0300

    Add parseWithRegex function to parse JSON with regular expressions

---
+++ functions/parseWithRegex.js
@@ -0,0 +1,16 @@
+/**
+ * Parses a string containing JSON with regular expressions.
+ * @param {string} str - The string to parse.
+ * @returns {any} - The parsed JSON object.
+ */
+export function parseWithRegex(str) {
+  function reviver(key, value) {
+      if (typeof value === 'string' && value.startsWith('__REGEXP ')) {
+          const match = value.split('__REGEXP ')[1].match(/\/(.*)\/(.*)?/);
+          return new RegExp(match[1], match[2] || '');
+      }
+      return value;
+  }
+
+  return JSON.parse(str, reviver);
+}
